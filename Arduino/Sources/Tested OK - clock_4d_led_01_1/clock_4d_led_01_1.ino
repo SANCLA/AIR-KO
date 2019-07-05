a3e418c4d23783d79ca5084cae99cc9a003a0975
github@sancla.com <372ac6fb4666c9a2264df92df552ed06>
2019-07-05 23:17:17 +0200

    ALPHA 0.2

---
+++ Arduino/Sources/Tested OK - clock_4d_led_01_1/clock_4d_led_01_1.ino
@@ -0,0 +1,67 @@
+/* name      : clock_4d_led_01_1.ino
+   purpose   : Clock with 4 digit LED display (TM1637), using Arduino and RTC DS1307
+   author    : LZ2WSG Veselin Georgiev
+   site      : http://kn34pc.com/construct/lz2wsg_arduino_clock_4d_led.html
+   date      : 2-12-2018
+   revision  : v1.01.1
+   used info : http://www.manelsoft.com/projects/arduino_ds1307_clock.aspx
+             : http://playground.arduino.cc/Main/TM1637
+             : http://www.ad7c.com/projects/ad9850-dds-vfo/
+  ---------------------------------------------------------------------------------------------------------*/
+// meh
+
+#include <Wire.h>
+#include "RTClib.h"                   // https://github.com/adafruit/RTClib
+#include "TM1637.h"                   // https://github.com/avishorp/TM1637
+
+#define CLK 2                         // TM1637 pin CLK
+#define DIO 3                         // TM1637 pin DIO
+
+TM1637 tm1637(CLK, DIO);
+RTC_DS1307 RTC;                       // RTC DS1307 use SDA(A4) and SCL(A5) pins
+
+uint8_t m;                            // minutes
+uint8_t h;                            // hours
+bool dp = true;                       // digital points
+
+void setup()
+{
+  Wire.begin();                       // Initiate the Wire library and join the I2C bus
+
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+     Serial.println("Serial I AM OK!!!"); // so I can keep track of what is loaded
+
+  RTC.begin();                        // Initiate the RTC library
+//  RTC.adjust(DateTime(__DATE__, __TIME__));
+
+  tm1637.init();
+  tm1637.set(BRIGHTEST);              // BRIGHT_TYPICAL = 2, BRIGHT_DARKEST = 0, BRIGHTEST = 7
+}
+
+
+void loop()
+{
+
+  DateTime now = RTC.now();
+  Serial.begin(115200);
+            Serial.print(now.hour(), DEC);
+            Serial.print(':');
+            Serial.print(now.minute(), DEC);
+            Serial.print(':');
+            Serial.print(now.second(), DEC);
+            Serial.println();
+
+  h = now.hour();
+  m = now.minute();
+
+  tm1637.display(0, h / 10);          // Time Format -> HH : MM
+  tm1637.display(1, h % 10);
+  tm1637.display(2, m / 10);
+  tm1637.display(3, m % 10);
+
+  tm1637.point(dp);
+  dp = !dp;                           // time refresh (and digital points blink) 0,5 every sec
+  
+  delay(500);
+}
--- README.md
+++ README.md
@@ -1,10 +1,45 @@
-### PROJECT DETAILS ###
+# PROJECT DETAILS #
 Project Name: AIR-K.O.
 
-Version: 0.1 ALPHA
+Version: 0.2 ALPHA
 
-Date: 12-6-2019
+Date: 5-7-2019
 
 Author(s): Sander Claassen
 
 Project description: Timed buzzer for remind folks to switch off the airco before leaving the room.
+
+Goals:
+
+- Aesthetically pleasing solution
+- Give a visual and audio alarm when it's 17:00 hours
+- Ability to acknowledge the alarm
+- Ability to adjust the volume
+- Ability to adjust the time
+- Accurate time display
+
+
+
+# Status
+
+| Part                                   | Status            | Remarks |
+| -------------------------------------- | ----------------- | ------- |
+| Visual concept                         | Done              |         |
+| Order all parts                        | Done              |         |
+| Arduino Nano                           | Working prototype |         |
+| Clock display 4 segment                | Working prototype |         |
+| RTC module                             | Working prototype |         |
+| Buttons to adjust the time             | To do             |         |
+| Red warning led                        | To do             |         |
+| Button to acknowledge the alarm        | To do             |         |
+| Buzzer                                 | To do             |         |
+| Way to adjust the volume of the buzzer | To do             |         |
+| Box to put it all into                 | To do             |         |
+
+
+
+# Credits
+
+- 7-segment clock + RTC
+  https://www.kn34pc.com/construct/lz2wsg_arduino_clock_4d_led.html
+- ...
